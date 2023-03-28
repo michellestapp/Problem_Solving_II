057dbabceaab916119a21f702d7d5eb2d40f45c8
michellestapp <mstapp@rocketmail.com>
2023-03-28 15:04:56 -0500

    feat: added two palindrome functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# Task 1: Reverse a String
-# (HINT: Review the Algorithms + Problem Solving Lecture!)
-# Write code that takes a string as input and returns the string reversed
-# i.e. “Hello” will be returned as “olleH”
+# # Task 1: Reverse a String
+# # (HINT: Review the Algorithms + Problem Solving Lecture!)
+# # Write code that takes a string as input and returns the string reversed
+# # i.e. “Hello” will be returned as “olleH”
 
 # def reverse_a_word():
 #     reversed_word = ''
@@ -15,23 +15,74 @@
 #     print(reversed_word)
     
 
-#reverse_a_word()
+# reverse_a_word()
 
-# Task 2: Capitalize a Letter
+# # Task 2: Capitalize a Letter
 
-# Write code that takes a string as input and capitalize the first letter of each word.
-#  Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
+# # Write code that takes a string as input and capitalize the first letter of each word.
+# #  Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
-def cap_first_letter():
-    completed_sentence = ''
-    sentence = input(" Enter a sentence string with no capitals:  ")
+# def cap_first_letter():
+#     completed_sentence = ''
+#     sentence = input(" Enter a sentence string with no capitals:  ")
 
-    words_in_sentence = sentence.split()
-    for word in words_in_sentence:
+#     words_in_sentence = sentence.split()
+#     for word in words_in_sentence:
        
-       completed_sentence += (word.capitalize() + ' ')
-       
-    print(completed_sentence)
+#        completed_sentence += word.capitalize() + ' '
+
+#     print(completed_sentence)
 
 
-cap_first_letter()
+# cap_first_letter()
+
+
+# Task 3: Palindrome
+# A “palindrome” is a word, phrase, or sequence that reads the same backward as forward i.e. madam	
+
+
+# Write code that takes a user input and checks to see if it is a Palindrome and reports the result
+
+def palindrome_one():
+    is_palindrome = 'True'
+    word = input(" Enter a word to be checked as a palindrome: ")
+
+    index = len(word) - 1
+
+    index_one = 0
+    index_two = index
+
+    while index_one in range(index):
+        
+        if word[index_one] == word[index_two]:
+            index_one += 1
+            index_two -= 1          
+            is_palindrome = 'True'
+        else:
+            is_palindrome = 'False'
+            break
+    
+    if is_palindrome == 'True':
+        print(f"  {word} is a palindrome! ")
+    else:
+        print(f"  {word} is not a palindrome")
+    print(word)
+
+
+palindrome_one()
+
+def palindrome_two():
+    reversed_word = ''
+    word = input(" Enter a word to verify if it is a palindrome: ")
+
+    num_index = len(word) - 1
+
+    for index in range(num_index, -1, -1):
+        reversed_word += word[index]
+
+    if word == reversed_word:
+        print(f"  {word} is a palindrome! ")
+    else:
+        print(f"  {word} is not a palindrome")
+
+palindrome_two()
